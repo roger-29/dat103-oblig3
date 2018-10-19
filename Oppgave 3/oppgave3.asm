c284f4a52678c2e4e1cc98121b6eb8b4b801b168
571556 <31258700+571556@users.noreply.github.com>
2018-10-19 22:01:35 +0200

    Inc to 10, dec to 0, print the var

--- Oppgave 3/oppgave3.asm
+++ Oppgave 3/oppgave3.asm
@@ -1,5 +1,5 @@
 section .bss
-    a resb 1
+    a resb 1            ;Plass til variabelen a
 
 section .data
     crlf db 13,10
@@ -10,43 +10,52 @@ section .text
 global _start
 
 _start:
-    mov [a],byte 0
-    mov ecx,20
-    call lokke
-    call printa
+        mov eax,0
+        mov [a],eax     ;variabelen a blir tildelt verdien 0.
+        mov ecx,0       ;Tellevariabelen i.
+        call nylinje    
+lokke:  cmp ecx,10      ;Starten av løkken.Sammenligner tellevariabelen med 10 (i < 10)
+        jb increment    ;Hvis i < 10 er sann så inkrementeres a
+        call decrement  ;Hvis i < 10 er usann så dekrementeres a
+lm:     cmp ecx,19      ;Sammenligner tellevariabelen med 20-1 for å sjekke om den er mindre
+        inc ecx         ;Tellevariabelen inkrementeres
+        jb lokke        ;Løkken itereres på nytt hvis tellevariabelen var mindre enn 
+        call print      ;20-1
+        call nylinje
+        mov eax,1
+        mov ebx,0       
+        int 0x80        ;Programmet avsluttes
 
-lokke:
-    call condition
-    cmp ecx,0
-    sub ecx,1
-    jne lokke
-
-condition:
-    cmp ecx,10
-    jl increment
-    call decrement
- 
-        
+;Inkrementerer variabelen a        
 increment:
-    lock inc byte [a]
-    ret
+    mov eax,[a]
+    inc eax
+    mov [a],eax
+    jmp lm  
 
+;Dekrementerer variabelen a
 decrement:
-    lock dec byte [a]
-    ret
+    mov eax,[a]
+    dec eax
+    mov [a],eax
+    jmp lm   
 
-printa:
-    mov edx,2
-    mov ecx,a
+;Printer variabelen lagre i a
+print:
+    push ebp
+    mov ebp,esp
+    mov edx,1
+    mov ecx,[a]
     add ecx,'0'
+    mov [a],ecx
+    mov ecx,a
     mov ebx,1
     mov eax,4
-    int 80h
-    call nylinje
-    mov ebx,0
-    mov eax,1
-    int 80h
+    int 0x80
+    pop ebp
+    ret
 
+;Hentet fra oppgave 2
 nylinje:
     push eax
     push ebx
@@ -56,7 +65,7 @@ nylinje:
     mov ecx,crlf
     mov ebx,1
     mov eax,4
-    int 80h
+    int 0x80
     pop edx
     pop ecx
     pop ebx
